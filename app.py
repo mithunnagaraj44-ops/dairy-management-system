b865c86700209c1a57f428ad92dedb12d506d160
Mithun <your-email@gmail.com>
2026-04-20 14:28:36 +0530

    fix syntax error in get_db

--- app.py
+++ app.py
@@ -8,14 +8,15 @@ app.secret_key = "secret123"
 
 
 def get_db():
-    return mysql.connector.connect(
-        host=os.getenv("MYSQLHOST"),
-        user=os.getenv("MYSQLUSER"),
-        password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT") or 3306)
-    )
-    except Error as e:
+    try:
+        return mysql.connector.connect(
+            host=os.getenv("MYSQLHOST"),
+            user=os.getenv("MYSQLUSER"),
+            password=os.getenv("MYSQLPASSWORD"),
+            database=os.getenv("MYSQLDATABASE"),
+            port=int(os.getenv("MYSQLPORT") or 3306)
+        )
+    except Exception as e:
         print("DB ERROR:", e)
         return None
 
